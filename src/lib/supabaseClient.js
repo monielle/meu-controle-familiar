32d3500587fb9aca6b8e62c3360cb347e918c51b
monielle <monielle@gmail.com>
2026-02-12 09:52:57 -0300

    Modificando arquivo supabase

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,12 @@
+
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+// Verificação de segurança para o console (ajuda a descobrir se a chave sumiu)
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("Erro: Chaves do Supabase não encontradas no ambiente!");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
